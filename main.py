6e7b98ef59803cb919fe5c48f87f1684067f8b5e
AndaBurger1234 <wasteuse235@gmail.com>
2025-02-12 17:54:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,6 @@ import yt_dlp
 import asyncio
 import os
 
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-
-@app.route('/')
-def home():
-  return "Keep Quiet --> Brimstone<br>(bot online btw..."
-
-
-def run():
-  app.run(host='0.0.0.0', port=8080)
-
-
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
-
 # Enable necessary Discord intents
 intents = discord.Intents.default()
 intents.message_content = True  # REQUIRED to read message content
